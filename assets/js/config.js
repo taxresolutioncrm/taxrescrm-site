7f4e97075deb67b4935225a4e511e0837b2ae8b6
Romy Cruz <romy@taxrescrm.net>
2026-08-01 03:51:38 +0000

    fix: BOOK_URL includes ?demo=true so booking page shows TaxRes CRM branding

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 window.TAXRES_CONFIG = {
   SITE_NAME: "TaxRes CRM",
   MARKETING_URL: "https://taxrescrm.net",
-  BOOK_URL: "https://taxresolutioncrm.github.io/taxcasereview-CRM/book",
+  BOOK_URL: "https://taxresolutioncrm.github.io/taxcasereview-CRM/book?demo=true",
   APP_URL: "https://taxresolutioncrm.github.io/taxcasereview-CRM/login",
   CONTACT_EMAIL: "romy@taxrescrm.net"
 };
