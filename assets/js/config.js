05540c9da904c6fb51be45b615c5e82fe570127c
Romy Cruz <romy@taxrescrm.net>
2026-08-06 19:08:30 +0000

    fix: missing comma in config.js broke all Book a Demo buttons; regenerate favicon from current logo

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,7 +3,7 @@ window.TAXRES_CONFIG = {
   MARKETING_URL: "https://taxrescrm.net",
   BOOK_URL: "https://taxresolutioncrm.github.io/taxcasereview-CRM/book?demo=true",
   APP_URL: "https://taxresolutioncrm.github.io/taxcasereview-CRM/login",
-  CONTACT_EMAIL: "info@taxrescrm.net"
+  CONTACT_EMAIL: "info@taxrescrm.net",
   GA4_ID: "",
   CLARITY_ID: "",
   BING_VERIFICATION: ""
